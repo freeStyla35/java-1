4befa5a423ca6a20828fea576250f40d1162d000
orcuncanlilar <orcuncanlilar@orcuns-mbp.lan>
2018-11-20 21:04:10 -0500

    My F	irst Program

---
+++ 1-My First Java Program/src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World");
+		System.out.println("My Name is Mike Smith");
+		System.out.print("How is everyone?");
+		System.out.println(" That's great!");
+		
+		
+
+	}
+
+}
