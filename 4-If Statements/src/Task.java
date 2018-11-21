6a828aa507f275d1c192aa2dc00af339ac0fc00d
orcuncanlilar <orcuncanlilar@orcuns-mbp.lan>
2018-11-20 21:15:35 -0500

    If Statements

---
+++ 4-If Statements/src/Task.java
@@ -0,0 +1,16 @@
+
+public class Task {
+
+	public static void main(String[] args) {
+		
+		int marks=10;
+		if(marks<60) {
+			System.out.println("Fail");
+		}else if(marks<90) {
+			System.out.println("Pass");
+		}else {
+			System.out.println("Passed with distinction");
+		}
+	}
+
+}
