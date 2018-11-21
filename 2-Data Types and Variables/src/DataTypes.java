d5afd6684bbefbbf955c28024ecd36b6a0f64c2b
orcuncanlilar <orcuncanlilar@orcuns-mbp.lan>
2018-11-20 21:11:23 -0500

    Data Types and Variables

---
+++ 2-Data Types and Variables/src/DataTypes.java
@@ -0,0 +1,81 @@
+
+public class DataTypes {
+
+	public static void main(String[] args) {
+		
+		//byte
+		byte b1=25;
+		byte b2=-128;
+		byte b3=127;
+		System.out.println(b1+"|"+b1+"|"+b3);
+		//byte b4=128;
+		
+		System.out.println("-------------------");
+		
+		//short
+		short s1=500;
+		short s2=-32768;
+		short s3=32767;
+		System.out.println(s1+"|"+s2+"|"+s3);
+		//short s4=32768;
+		
+		System.out.println("-------------------");
+		
+		//int
+		int i1=-50000;
+		int i2=50000;
+		int i3=100_000;
+		System.out.println(i1+"|"+i2+"|"+i3);
+		
+		System.out.println("-------------------");
+		
+		//long
+		long l1=-50000;
+		long l2=100_000_000;
+		long l3=214783949403l;
+		System.out.println(l1+"|"+l2+"|"+l3);
+		
+		System.out.println("-------------------");
+		
+		//float; floating point types have double as default type
+		float f1=5.27f;
+		double d1=5.27;
+		
+		System.out.println("-------------------");
+		
+		//char
+		char c1='D';
+		char c2='2';
+		char c3='@';
+		System.out.println(c1+"|"+c2+"|"+c3);
+
+		
+		//unicode-table.com
+		char myChar='\u00A9';
+		char registeredSymbol='\u00AE';
+		System.out.println(myChar + "|" + registeredSymbol);
+		
+		System.out.println("-------------------");
+		
+		//boolean
+		boolean bool1=true;
+		boolean bool2=false;
+		System.out.println(bool1+"|"+bool2);
+
+
+		
+		
+
+		
+
+		
+
+		
+		
+		
+		
+		
+
+	}
+
+}
---
+++ 2-Data Types and Variables/src/Variables.java
@@ -0,0 +1,24 @@
+
+public class Variables {
+
+	public static void main(String[] args) {
+		
+//		String s1,s2;
+//		s1="Mike";
+//		s2="Smith";
+		
+		String s1="Mike",s2;
+		s2="Smith";
+		
+//		int i1,i2,i3=0;
+//		System.out.println(i1+"|"+i2+"|"+i3);
+		
+//		int num,String value;
+//		int num;String value;
+		
+		
+		
+		
+	}
+
+}
